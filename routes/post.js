67cb9cdde11bfe503ffa73e937d3351cef431a5f
marco <serranomarco@hotmail.com>
2020-06-15 15:40:55 -0700

    Finish getting and creating a post

--- routes/post.js
+++ routes/post.js
@@ -1,16 +1,32 @@
 const express = require('express');
-
-// const db = require('')
+const { asyncHandler } = require('../utils');
+const db = require('../db/models');
 
 const router = express.Router();
 
-router.get('/posts/:postId(\\d+)', (req, res) => {
-  //TODO
-});
+//get a post
+router.get('/posts/:postId(\\d+)', asyncHandler(async (req, res) => {
+  const postId = req.params.postId;
+  const post = await db.Post.findByPk(postId);
+  res.json({ post });
+}));
 
-router.post('/posts', (req, res) => {
-  //TODO
-});
+//create a post
+router.post('/posts', asyncHandler(async (req, res) => {
+  const {
+    caption,
+    photoPath
+  } = req.body;
+  console.log(req.body);
+
+  const post = await db.Post.create({
+    userId: 1, // replace with req.user.id so it's based on who is logged in
+    caption,
+    photoPath
+  });
+
+  res.json({ post });
+}));
 
 router.put('/posts/postId(\\d+)', (req, res) => {
   //TODO
@@ -24,9 +40,14 @@ router.get('/posts/user/:userId(\\d+)', (req, res) => {
   //TODO
 });
 
-router.get('/posts/following/:userId(\\d+)', (req, res) => {
-  //TODO
-});
+router.get('/posts/following/:userId(\\d+)', asyncHandler(async (req, res) => {
+  // const userId = req.params.userId
+  // console.log('this is the user id', userId)
+  // const posts = await db.Post.findAll({ where: { followerId: userId } });
+  // // , order: [['createdBy', 'DESC']] }
+  // console.log(posts);
+  // res.json({ posts });
+}));
 
 router.post('/posts/:postId(\\d+)/comments', (req, res) => {
   //TODO
